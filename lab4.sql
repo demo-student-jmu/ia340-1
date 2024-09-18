85df07fbb95e462b073df8c481af7f7c94395334
demo student <weixuebingcp@gmail.com>
2024-09-18 16:39:27 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,22 @@
+--4.1
+/* no, because we still have VA records */
+
+
+--4.2
+/* no, because 80 is not a valid fisp*/
+
+--4.3
+/* just fine */
+
+--4.6
+/* it works pefect*/
+
+SELECT name.name, income.income, income.year
+FROM income
+JOIN name ON income.fips = name.fips
+WHERE income.year = (SELECT MAX(year) FROM income)
+ORDER BY income.income DESC
+LIMIT 1;
+
+
 
